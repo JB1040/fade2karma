f2e89b059f5a3259491ccd2e461b697c0a0b6446
Karlis Petersons <karlis039@gmail.com>
2017-08-14 20:12:45 +0200

    navigation bar check if there is a giveaway if not dont show it if is then link to it...

--- src/app/navigation.component.ts
+++ src/app/navigation.component.ts
@@ -1,6 +1,8 @@
-﻿import { Component, ElementRef, HostListener } from '@angular/core';
+﻿import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { NavigationService, NavItem } from './navigation.service';
+import { BASE_URL } from './core/globals';
+import { Http } from '@angular/http';
 
 @Component({
     selector: 'f2kNavigation',
@@ -8,10 +10,12 @@ import { NavigationService, NavItem } from './navigation.service';
     styleUrls: ['./navigation.component.css']
 })
 
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
     open: boolean;
     lastScrollTop: number;
     top: number;
+    giveawayExists = false;
+    giveawayUrl: string;
     navItems: NavItem[] = [
         // new NavItem('Decklists', ['Hearthstone', 'Gwent']),
         new NavItem('Tier List', []),
@@ -36,7 +40,12 @@ export class NavigationComponent {
     constructor(private service: NavigationService,
                 private route: ActivatedRoute,
                 private router: Router,
-                private element: ElementRef) {
+                private element: ElementRef,
+                private http: Http) {
+    }
+
+    ngOnInit() {
+        this.setFeatured();
     }
 
     toLink(url: string) {
@@ -59,5 +68,15 @@ export class NavigationComponent {
         let currentRoute: string | string[] = this.router.url;
         currentRoute = currentRoute.split('/');
         return currentRoute[currentRoute.length - 1] === this.toLink(name);
+    }
+
+    setFeatured(): void { // TODO move in service, handle errors in case they take place...
+        this.http.get(`${BASE_URL}/api/articles/featured`).subscribe(res => {
+            const articles = res.json();
+            if (articles > 1) {
+                this.giveawayUrl = `/articles/${articles[1].title.replace(/ /g, '_').toLowerCase()}_${articles[1].id}`;
+                this.giveawayExists = true;
+            }
+        });
     }
 }
