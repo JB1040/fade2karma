18e61d4a3c0d93dd4203c9417527b0c0ce4b5609
JB940 <Jordybr94@gmail.com>
2017-08-17 20:35:51 +0200

    fixed all video now

--- src/app/articles/article/article-content/article-content.component.ts
+++ src/app/articles/article/article-content/article-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, ElementRef, HostListener } from '@angular/core';
 import { Article } from '../../article';
 import { DomSanitizer} from '@angular/platform-browser';
 
@@ -12,13 +12,24 @@ export class ArticleContentComponent implements OnInit {
     @Input() article: Article;
     @Input() articles: Article[];
     CONTENT: any;
+	IMAGE: any;
+	height: any;
 
-    constructor(private sanitizer: DomSanitizer) { }
+    constructor(private sanitizer: DomSanitizer, private el: ElementRef) { }
+
+	@HostListener('window:resize', ['$event.target']) 
+	onResize() { 
+		this.resizeWorks();
+	}
+	
+
+	private resizeWorks(): void {
+		this.height = this.el.nativeElement.width * 0.667;
+	}
 
     ngOnInit() {
         if (this.article.imageURL && (this.article.imageURL.indexOf('youtube') !== -1 || this.article.imageURL.indexOf('twitch') !== -1)) {
-			
-            this.CONTENT = this.sanitizer.bypassSecurityTrustHtml("<div class=\"box\"><iframe src='" + this.article.imageURL+"'></iframe></div>" + this.article.content);
+			 this.CONTENT = this.sanitizer.bypassSecurityTrustHtml("<iframe src='" + this.article.imageURL+"'></iframe>" + this.article.content);
         } else if (this.article.imageURL) {
             this.CONTENT = `<img src="${this.article.imageURL.indexOf('http') !== -1 ? this.article.imageURL : 'assets/images/' + this.article.imageURL}">${this.article.content}`;
         } else {
