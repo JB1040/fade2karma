8f6e6d350c8712d3b8c0239533f4bf6d912e5cfe
Karlis Petersons <karlis039@gmail.com>
2017-08-19 17:43:09 +0200

    image display changes

--- src/app/teased/teased-tile.component.ts
+++ src/app/teased/teased-tile.component.ts
@@ -20,8 +20,7 @@ export class RecommendedTileComponent implements OnInit {
 
     @Output() width = new EventEmitter<number>();
 
-    @HostListener('click')
-    onClick() {
+    @HostListener('click') onClick() {
         if (this.type = 'deck') {
             this.router.navigate([`/tier_list/${this.teasedItem.title.replace(/ /g, '_').replace(/[^a-zA-Z0-9;,+*()\'$!-._~?/]/g, '').toLowerCase()}_${this.teasedItem.id}`]);
         } else {
