ea5d170127a2cba3a16b5d06f97391336b138b5f
Karlis Petersons <karlis039@gmail.com>
2017-09-04 21:25:54 +0200

    - google analytics - facebook pixel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 ﻿import {Component} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
+import {GoogleAnalyticsEventsService} from './google-analytics-events.service';
 
 @Component({
     selector: 'f2kApp',
@@ -11,5 +13,16 @@
     `,
 })
 export class AppComponent {
+    constructor(public router: Router, public googleAnalyticsEventsService: GoogleAnalyticsEventsService) {
+        this.router.events.subscribe(event => {
+            if (event instanceof NavigationEnd) {
+                ga('set', 'page', event.urlAfterRedirects);
+                ga('send', 'pageview');
+            }
+        });
+    }
 
+    submitEvent() {
+        this.googleAnalyticsEventsService.emitEvent('testCategory', 'testAction', 'testLabel', 0);
+    }
 }
