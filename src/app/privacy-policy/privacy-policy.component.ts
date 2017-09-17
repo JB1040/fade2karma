35c3dfd63f9239320166c5d6f32053e4d989da77
Karlis Petersons <karlis039@gmail.com>
2017-09-17 22:03:33 +0200

    added about-us, privacy-policy, terms-of-service pages

---
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'f2kPrivacyPolicy',
+  templateUrl: './privacy-policy.component.html',
+  styleUrls: ['./privacy-policy.component.css']
+})
+export class PrivacyPolicyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/teams/teams-routing.module.ts
+++ src/app/teams/teams-routing.module.ts
@@ -8,17 +8,23 @@ import { HomePageComponent } from '../home/home-page.component';
 import { TierListHubComponent } from '../tier-list-hub/tier-list-hub.component';
 import { NewDeckHubComponent } from '../decks/new-deck-hub/new-deck-hub.component';
 import { GiveawaysHubComponent } from '../giveaways/hub/giveaways-hub.component';
-import {DmcaComponent} from '../dmca/dmca.component';
+import { DmcaComponent } from '../dmca/dmca.component';
+import { PrivacyPolicyComponent } from '../privacy-policy/privacy-policy.component';
+import { TermsOfServiceComponent } from '../terms-of-service/terms-of-service.component';
+import { AboutUsComponent } from '../about-us/about-us.component';
 
 const contentRoutes: Routes = [
-    {path: 'team', component: TeamsComponent},
-    {path: '', component: HomePageComponent},
-    {path: 'articles', component: ArticlesComponent},
-    {path: 'articles/:article', component: ArticleComponent},
-    {path: 'tier_list/:deck', component: NewDeckHubComponent},
-    {path: 'tier_list', component: TierListHubComponent},
-    {path: 'giveaways', component: GiveawaysHubComponent},
-    {path: 'dmca', component: DmcaComponent}
+    { path: 'team', component: TeamsComponent },
+    { path: '', component: HomePageComponent },
+    { path: 'articles', component: ArticlesComponent },
+    { path: 'articles/:article', component: ArticleComponent },
+    { path: 'tier_list/:deck', component: NewDeckHubComponent },
+    { path: 'tier_list', component: TierListHubComponent },
+    { path: 'giveaways', component: GiveawaysHubComponent },
+    { path: 'dmca', component: DmcaComponent },
+    { path: 'privacy_policy', component: PrivacyPolicyComponent },
+    { path: 'terms_of_service', component: TermsOfServiceComponent },
+    { path: 'about', component: AboutUsComponent }
 ];
 
 @NgModule({
