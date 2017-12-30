db9c038075c0f1d2ac79461a06a9468330a4dfc9
Karlis Petersons <karlis039@gmail.com>
2017-12-30 13:11:41 +0100

    added angular service worker

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule)
+    .then(() => {
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('/ngsw-worker.js');
+        }
+    })
+    .catch(err => console.error(err));
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,5 +1,12 @@
 /* SystemJS module definition */
 declare var module: NodeModule;
+
 interface NodeModule {
-  id: string;
+    id: string;
 }
+
+declare interface Window {
+    adsbygoogle: any[];
+}
+
+declare var adsbygoogle: any[];
